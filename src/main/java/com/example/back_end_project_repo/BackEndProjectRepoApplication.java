6f3f9c0ef7d93ebb7b3aab8a4dbe67139465a325
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-12-10 16:19:39 -0500

    initial commit

---
+++ src/main/java/com/example/back_end_project_repo/BackEndProjectRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.back_end_project_repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndProjectRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndProjectRepoApplication.class, args);
+	}
+
+}
