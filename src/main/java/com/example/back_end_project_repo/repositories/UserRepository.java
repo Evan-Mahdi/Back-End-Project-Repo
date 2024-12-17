2229830aaa3d7aaac2186f48516f2e90de00c2dd
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-12-17 14:48:34 -0500

    User controller added with login and register method

--- src/main/java/com/example/back_end_project_repo/repositories/UserRepository.java
+++ src/main/java/com/example/back_end_project_repo/repositories/UserRepository.java
@@ -3,5 +3,9 @@ package com.example.back_end_project_repo.repositories;
 import com.example.back_end_project_repo.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+
 }
