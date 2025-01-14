a15261adcc7e7dc3bd63c4f7e0d5b332b17a24a9
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2025-01-14 16:41:19 -0500

    seeding data with json file automatically for User and Event tables added

--- src/main/java/com/example/back_end_project_repo/BackEndProjectRepoApplication.java
+++ src/main/java/com/example/back_end_project_repo/BackEndProjectRepoApplication.java
@@ -1,13 +1,63 @@
 package com.example.back_end_project_repo;
 
+import com.example.back_end_project_repo.services.EventServices;
+import com.example.back_end_project_repo.services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @SpringBootApplication
-public class BackEndProjectRepoApplication {
+public class BackEndProjectRepoApplication implements CommandLineRunner {
+    @Autowired
+    UserServices userServices;
+    @Autowired
+    EventServices eventServices;
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackEndProjectRepoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndProjectRepoApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) {
+        try {
+//            eventServices.cleanUpEvebtTable();
+//            userServices.cleanUpUserTable();
+
+            // Load JSON files from classpath (assets folder in resources)
+            ClassPathResource userResources = new ClassPathResource("users.json");
+            ClassPathResource eventResource = new ClassPathResource("events.json");
+
+            // Check if the resources exist
+            if (!userResources.exists()) {
+                System.out.println("Device JSON file not found.");
+                return;
+            }
+            if (!eventResource.exists()) {
+                System.out.println("Cart JSON file not found.");
+                return;
+            }
+
+            // Read the JSON data
+            try (InputStream userStream = userResources.getInputStream()) {
+                userServices.importJsonData(userStream);
+            } catch (IOException e) {
+                System.out.println("Error reading User JSON: " + e.getMessage());
+            }
+
+            try (InputStream eventStream = eventResource.getInputStream()) {
+                eventServices.importJsonData(eventStream);
+            } catch (IOException e) {
+                System.out.println("Error reading event JSON: " + e.getMessage());
+            }
+
+            System.out.println("Data imported from JSON files successfully.");
+        } catch (Exception e) {
+            System.out.println("An error occurred during the data import process: " + e.getMessage());
+        }
+    }
 }
--- src/main/java/com/example/back_end_project_repo/models/Event.java
+++ src/main/java/com/example/back_end_project_repo/models/Event.java
@@ -23,38 +23,49 @@ public class Event {
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public String getDate() {
         return date;
     }
+
     public void setDate(String date) {
         this.date = date;
     }
+
     public String getTime() {
         return time;
     }
+
     public void setTime(String time) {
         this.time = time;
     }
+
     public Boolean getAllDay() {
         return allDay;
     }
+
     public void setAllDay(Boolean allDay) {
         this.allDay = allDay;
     }
-    
+
 }
